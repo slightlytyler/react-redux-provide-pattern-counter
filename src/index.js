4835944e31e8751d962f3342656731728802d4da
Tyler Martinez <slightlytyler@gmail.com>
2016-02-26 15:04:15 -0600

    Add DevTools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer';
+import DevTools from './DevTools';
 import App from './components/App';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(rootReducer);
+const initialState = {};
+const enhancer = compose(
+  applyMiddleware(thunk),
+  DevTools.instrument(),
+);
+
+const store = createStore(rootReducer, initialState, enhancer);
+
+if (module.hot) {
+  module.hot.accept('./rootReducer', () =>
+    store.replaceReducer(require('./rootReducer'))
+  );
+}
 
 render(
   <Provider store={store}>
