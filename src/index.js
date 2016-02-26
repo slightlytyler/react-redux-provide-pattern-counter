9856b6c1d20edb58e6f28b8c7b7d23ba87b89f91
Tyler Martinez <slightlytyler@gmail.com>
2016-02-26 14:48:37 -0600

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './rootReducer'
-import App from './components/App'
+import rootReducer from './rootReducer';
+import App from './components/App';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(rootReducer);
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import countProvider from './providers/count.js';
+import countProvider from './providers/count';
 
 export default combineReducers({
   count: countProvider.reducers,
